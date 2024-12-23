29222367a44b6b71bccb19740a31dc9b76cc066a
Yonas <yonasmilo@gmail.com>
2024-12-23 11:08:37 -0500

    fsc-00 Initial commit

---
+++ src/main/java/com/yonasamare/fsc/FscApplication.java
@@ -0,0 +1,13 @@
+package com.yonasamare.fsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yonasamare/fsc/FscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonasamare.fsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
